3be2dbde8a54c644b2d4b0b7be73880f0174b3d4
dpbaia <diana.baiadori@gmail.com>
2022-01-12 10:32:57 +0100

    logic of arrows to move between images is working

--- amigurumis/static/amigurumis/main.js
+++ amigurumis/static/amigurumis/main.js
@@ -22,11 +22,42 @@ function changeImage(param) {
 function rightArrowChangeImage(){
     // Get the current active image
     let currentStep = document.querySelector(".img-active").parentElement.classList[0]
-    console.log(currentStep);
     // Find its step number
-    let nextstep = parseInt(currentStep) + 1
-    console.log(nextstep);
+    let nextStep = parseInt(currentStep) + 1
     // Get the next step
+    let nextElementParent = document.querySelector(`.\\3${nextStep}`);
     // Substitute with the new value 
-    document.querySelector(".img-active").classList.remove("img-active"); 
+    if(nextElementParent){
+        let nextElement = nextElementParent.firstElementChild
+        document.querySelector(".img-active").classList.remove("img-active");
+        nextElement.classList.add("img-active");
+        document.getElementById("slideshow").src = nextElement.src;
+    } else {
+        let nextElement = document.querySelector(".\\31").firstElementChild;
+        document.querySelector(".img-active").classList.remove("img-active");
+        nextElement.classList.add("img-active");
+        document.getElementById("slideshow").src = nextElement.src;
+    }
+}
+
+function leftArrowChangeImage(){
+    // Get the current active image
+    let currentStep = document.querySelector(".img-active").parentElement.classList[0]
+    // Find its step number
+    let nextStep = parseInt(currentStep) - 1
+    // Get the next step
+    let nextElementParent = document.querySelector(`.\\3${nextStep}`);
+    // Substitute with the new value 
+    if(nextElementParent){
+        let nextElement = nextElementParent.firstElementChild
+        document.querySelector(".img-active").classList.remove("img-active");
+        nextElement.classList.add("img-active");
+        document.getElementById("slideshow").src = nextElement.src;
+    } else {
+        let numberImgSlide = document.getElementsByClassName("img-slide").length
+        let nextElement = document.querySelector(`.\\3${numberImgSlide}`).firstElementChild;
+        document.querySelector(".img-active").classList.remove("img-active");
+        nextElement.classList.add("img-active");
+        document.getElementById("slideshow").src = nextElement.src;
+    }
 }
