56216ff8b96a5d16368fa4300c30d37a674cd649
dpbaia <diana.baiadori@gmail.com>
2022-01-17 17:18:24 +0100

    made changes related to security

--- tgy_site/settings.py
+++ tgy_site/settings.py
@@ -171,12 +171,12 @@ cloudinary.config(
 
 # PRODUCTION
 # To avoid transmitting sensitive information accidentally over the HTTP
-# CSRF_COOKIE_SECURE = True
-# SESSION_COOKIE_SECURE = True
-# SECURE_SSL_REDIRECT = True
-# SECURE_HSTS_SECONDS = 31536000 # One year
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000 # One year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
